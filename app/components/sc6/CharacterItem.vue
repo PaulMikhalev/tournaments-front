9fcc9f3c610b10ccbdc82afbf11b9dff618b0023
p.mikhalev <paul.s.mikhalev@gmail.com>
2025-09-24 01:17:36 +0300

    fix: исправлено масштабирование аватаров персонажей
    - Пересчитан background-size спрайта с 1085x775px до 784x560px
    - Обновлены все background-position позиции с шагом 112px вместо 155px
    - Аватары теперь корректно отображаются в блоках 112px без искажений
    - Сохранен размер блоков size-28 без изменений

--- app/components/sc6/CharacterItem.vue
+++ app/components/sc6/CharacterItem.vue
@@ -1,6 +1,6 @@
 <template>
     <div 
-      class="size-28 bg-gray-200 text-black flex items-center justify-center text-center p-2 cursor-pointer hover:bg-gray-300 character-sprite"
+      class="relative size-28 text-black flex items-center justify-center text-center p-2 cursor-pointer character-sprite"
       :class="character ? `char-${character.toLowerCase()}` : 'char-null'"
       :title="character ? getCharacterName(character) : 'Random'"
     >
@@ -17,53 +17,53 @@ const props = defineProps<{
 
 </script>
 
-<style scoped lang="scss">
+<style scoped>
 .character-sprite {
   background-image: url('/sc6/chars.png');
-  background-size: 1085px 775px; // 7 * 155px, 5 * 155px
+  background-size: 784px 560px; /* 1085 * (112/155) = 784px, 775 * (112/155) = 560px */
   background-repeat: no-repeat;
   background-position: 0 0;
 }
 
-// 1 строка (0-4 позиции)
+/* // 1 строка (0-4 позиции) */
 .char-cassandra { background-position: 0px 0px; }
-.char-siegfried { background-position: -155px 0px; }
-.char-zasalamel { background-position: -310px 0px; }
-.char-nightmare { background-position: -465px 0px; }
-.char-amy { background-position: -620px 0px; }
+.char-siegfried { background-position: -112px 0px; }
+.char-zasalamel { background-position: -224px 0px; }
+.char-nightmare { background-position: -336px 0px; }
+.char-amy { background-position: -448px 0px; }
 
-// 2 строка (5-11 позиции)
-.char-2b { background-position: 0px -155px; }
-.char-xianghua { background-position: -155px -155px; }
-.char-maxi { background-position: -310px -155px; }
-.char-sophitia { background-position: -465px -155px; }
-.char-astaroth { background-position: -620px -155px; }
-.char-ivy { background-position: -775px -155px; }
-.char-tira { background-position: -930px -155px; }
+/* // 2 строка (5-11 позиции) */
+.char-2b { background-position: 0px -112px; }
+.char-xianghua { background-position: -112px -112px; }
+.char-maxi { background-position: -224px -112px; }
+.char-sophitia { background-position: -336px -112px; }
+.char-astaroth { background-position: -448px -112px; }
+.char-ivy { background-position: -560px -112px; }
+.char-tira { background-position: -672px -112px; }
 
-// 3 строка (12-18 позиции)
-.char-hilde { background-position: 0px -310px; }
-.char-yoshimitsu { background-position: -155px -310px; }
-.char-kilik { background-position: -310px -310px; }
-.char-groh { background-position: -620px -310px; }
-.char-cervantes { background-position: -775px -310px; }
-.char-setsuka { background-position: -930px -310px; }
+/* // 3 строка (12-18 позиции) */
+.char-hilde { background-position: 0px -224px; }
+.char-yoshimitsu { background-position: -112px -224px; }
+.char-kilik { background-position: -224px -224px; }
+.char-groh { background-position: -448px -224px; }
+.char-cervantes { background-position: -560px -224px; }
+.char-setsuka { background-position: -672px -224px; }
 
-// 4 строка (19-25 позиции)
-.char-haohmaru { background-position: 0px -465px; }
-.char-talim { background-position: -155px -465px; }
-.char-mitsurugi { background-position: -310px -465px; }
-.char-geralt { background-position: -465px -465px; }
-.char-taki { background-position: -620px -465px; }
-.char-azwel { background-position: -775px -465px; }
-.char-hwang { background-position: -930px -465px; }
+/* // 4 строка (19-25 позиции) */
+.char-haohmaru { background-position: 0px -336px; }
+.char-talim { background-position: -112px -336px; }
+.char-mitsurugi { background-position: -224px -336px; }
+.char-geralt { background-position: -336px -336px; }
+.char-taki { background-position: -448px -336px; }
+.char-azwel { background-position: -560px -336px; }
+.char-hwang { background-position: -672px -336px; }
 
-// 5 строка (26-28 позиции)
-.char-seong_min_na { background-position: 0px -620px; }
-.char-raphael { background-position: -155px -620px; }
-.char-voldo { background-position: -310px -620px; }
+/* // 5 строка (26-28 позиции) */
+.char-seong_min_na { background-position: 0px -448px; }
+.char-raphael { background-position: -112px -448px; }
+.char-voldo { background-position: -224px -448px; }
 
-// Специальные случаи для null (Random)
+/* // Специальные случаи для null (Random) */
 .char-null {
   background: #ef4444;
   color: white;
